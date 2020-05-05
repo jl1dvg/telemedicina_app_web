4804d15c7a89a9b662696adaca225d9e8a2fa704
jl1dvg <JorgeAMI2018>
2020-05-05 13:22:46 -0500

    Nueva ventana login y Home Page corregido

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:Skype_clone/provider/image_upload_provider.dart';
 import 'package:Skype_clone/provider/user_provider.dart';
-<<<<<<< HEAD
 import 'package:Skype_clone/resources/auth_methods.dart';
-=======
 import 'package:Skype_clone/resources/firebase_repository.dart';
->>>>>>> 8d3b72fdf2b716d41b68ec03e4bb3e102d0f49cb
 import 'package:Skype_clone/screens/home_screen.dart';
 import 'package:Skype_clone/screens/login_screen.dart';
 import 'package:Skype_clone/screens/search_screen.dart';
@@ -24,35 +21,26 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-=======
 
->>>>>>> 8d3b72fdf2b716d41b68ec03e4bb3e102d0f49cb
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ImageUploadProvider()),
         ChangeNotifierProvider(create: (_) => UserProvider()),
       ],
       child: MaterialApp(
-<<<<<<< HEAD
         title: "ConsulMed",
-=======
         title: "Skype Clone",
->>>>>>> 8d3b72fdf2b716d41b68ec03e4bb3e102d0f49cb
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: {
           '/search_screen': (context) => SearchScreen(),
         },
-<<<<<<< HEAD
         theme: ThemeData(brightness: Brightness.light),
         home: FutureBuilder(
           future: _authMethods.getCurrentUser(),
-=======
         theme: ThemeData( brightness: Brightness.dark),
         home: FutureBuilder(
           future: _repository.getCurrentUser(),
->>>>>>> 8d3b72fdf2b716d41b68ec03e4bb3e102d0f49cb
           builder: (context, AsyncSnapshot<User> snapshot) {
             if (snapshot.hasData) {
               return HomeScreen();
